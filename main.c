0200e189547646281bc2168ba1879d62b5a708e4
Qiyu Li <QiyuLi@users.noreply.github.com>
2013-10-10 03:01:54 -0400

    Pass in void * data in main.c

--- main.c
+++ main.c
@@ -10,12 +10,12 @@
 
 int main()
 {
-	int x=7;
+	void *x= (void *) 7;
 	__asm__ __volatile__ ("sub $0x4,%esp");
-	__asm__ __volatile__ ("movl $0x7,(%esp)");
-//	__asm__ __volatile__ ("push %0"
-//				:
-//				: "r" (x)	);
+//	__asm__ __volatile__ ("movl $0x7,(%esp)");
+	__asm__ __volatile__ ("movl %0, (%%esp)"
+				: 
+				: "r"(x)	);
         __asm__ __volatile__ ("call __lwt_trampoline_test");
         return 0;
 }
