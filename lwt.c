6a12663264c0c586cfb0660a1beeecdd787d97e6
Qiyu Li <QiyuLi@users.noreply.github.com>
2013-10-09 21:46:28 -0400

    Add all basic lwt_functions

--- lwt.c
+++ lwt.c
@@ -4,12 +4,54 @@
 
 #include <lwt.h>
 
-int __lwt_start()
+/* lwt functions */
+lwt_t lwt_create(lwt_fn_t fn, void *data)
+{
+	return NULL;
+}
+
+void *lwt_join(lwt_t lwt)
+{
+        return;
+}
+
+void lwt_die(void *val)
+{
+        return;
+}
+
+int lwt_yield(lwt_t lwt)
+{
+        return NULL;
+}
+
+lwt_t lwt_current(void)
+{
+        return NULL;
+}
+
+int lwt_id(lwt_t lwt)
+{
+        return NULL;
+}
+
+/* private lwt functions */
+void __lwt_schedule(void)
+{
+        return;
+}
+
+void __lwt_dispatch(lwt_t next, lwt_t current)
+{
+        return;
+}
+
+void __lwt_start()
 {
 	printf("lwt start!\n");
 }
 
-int __lwt_start_test()
+void __lwt_start_test()
 {
 	__lwt_start();
 }
--- lwt.h
+++ lwt.h
@@ -1,4 +1,36 @@
 #define DEFAULT_STACK_SIZE 16384 // 16KB
 
-int __lwt_start();
-int __lwt_start_test();
+/* Thread Status */
+#define ACTIVE          0x00
+#define INTERRUPTED     0x01
+#define KILLED          0x10
+
+typedef int lwt_t;
+
+
+typedef struct {
+        void *  stack;
+        int     tid;
+        int     status;
+        } lwt_tcb;
+
+
+typedef void *(*lwt_fn_t)(void *);
+
+/* lwt functions */
+lwt_t lwt_create(lwt_fn_t fn, void *data);
+void *lwt_join(lwt_t lwt);
+void lwt_die(void *val);
+int lwt_yield(lwt_t lwt);
+lwt_t lwt_current(void);
+int lwt_id(lwt_t lwt);
+
+/* private lwt functions */
+void __lwt_schedule(void);
+void __lwt_dispatch(lwt_t next, lwt_t current);
+extern void __lwt_trampoline();
+extern void __lwt_trampoline_test();
+void __lwt_start();
+void __lwt_start_test();
+
+
